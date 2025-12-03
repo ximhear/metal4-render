ce6919e8f45a8ea065a84799ddfdc55f5b9134d7
C.H Lee <ximhear@gmail.com>
2025-12-04 02:10:30 +0900

    Initial Commit

---
+++ Metal4Render/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Metal4Render
+//
+//  Created by gzonelee on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Metal4Render/Metal4RenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal4RenderApp.swift
+//  Metal4Render
+//
+//  Created by gzonelee on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Metal4RenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
